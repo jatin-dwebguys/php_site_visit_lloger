278f94af1ec5bf89e3145824403b79e03b2185d5
at VCC <lamdt@vccloud.vn>
2013-07-08 11:52:44 +0700

    edit color

--- to_img.php
+++ to_img.php
@@ -17,8 +17,8 @@
     $height = imagefontheight($font)+2;
 
     $image = imagecreatetruecolor ($width,$height);
-    $white = imagecolorallocate ($image,255,255,255);
-    $black = imagecolorallocate ($image,0,0,0);
+    $white = imagecolorallocate ($image,20,20,20);
+    $black = imagecolorallocate ($image,255,255,255);
     imagefill($image,0,0,$white);
 
     imagestring ($image,$font,0,0,$string,$black);
